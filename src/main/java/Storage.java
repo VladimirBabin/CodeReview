3b8ee68ec014913a564569aed9783619db8e60e7
VladimirBabin <vladimir.sol.rojo@gmail.com>
2022-04-07 15:56:23 +0300

    updated Storage class and StorageTest UnitTests, added deleteAll method

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -6,18 +6,48 @@ public class Storage {
     public static Map<Object, Object> storage = new HashMap<>();
 
     public Object get(Object key) {
-        return storage.get(key);
+        if (key == null) {
+            return "Key should not be null";
+        } else if (key.toString().isBlank()) {
+            return "Key should not be empty/blank";
+        } else if (storage.containsKey(key)) {
+            return storage.get(key);
+        }
+        return "nil";
     }
 
-    public void set(Object key, Object value) {
+    public String set(Object key, Object value) {
+        if (key == null) {
+            return "Key should not be null";
+        } else if (key instanceof String) {
+            String str = (String) key;
+            if (str.isBlank()) {
+                return "Key should not be empty/blank";
+            }
+        } else if (storage.containsKey(key)) {
+            storage.put(key, value);
+            return "Key overwritten";
+        }
         storage.put(key, value);
+        return "OK";
     }
 
     public Object delete(Object key) {
-        return storage.remove(key);
+        if (key == null) {
+            return "Key should not be null";
+        } else if (key.toString().isBlank()) {
+            return "Key should not be empty/blank";
+        } else if (storage.containsKey(key)) {
+            return storage.remove(key);
+        }
+        return "nil";
     }
 
     public Set<Object> keys() {
         return storage.keySet();
+    }
+
+    public void deleteAll() {
+        storage.clear();
     }
 }
--- src/test/java/StorageTest.java
+++ src/test/java/StorageTest.java
@@ -35,6 +35,15 @@ public class StorageTest {
         Assert.assertEquals(storage.get(123), "one-two-three");
         Assert.assertEquals(storage.get(listOfIntegers), listOfStrings);
         Assert.assertEquals(storage.get("server:name"), "fido");
+
+        // если ключ равен null, пустой или не существует
+
+        Assert.assertEquals("Key should not be null", storage.get(null));
+        Assert.assertEquals("Key should not be empty/blank", storage.get(" "));
+        Assert.assertEquals("Key should not be empty/blank", storage.get(""));
+        Assert.assertEquals("nil", storage.get("foo"));
+
+        storage.deleteAll();
     }
 
     @Test
@@ -66,7 +75,20 @@ public class StorageTest {
 
         // проверяем что они равны
 
-        Assert.assertEquals(Storage.storage, expectedMap);
+        Assert.assertEquals(expectedMap, Storage.storage);
+
+        // проверяем правильную работу метода если ключ равен null, пустой, уже существует, не существует
+
+        Assert.assertEquals("Key should not be null", storage.set(null, null));
+        Assert.assertEquals("Key should not be empty/blank", storage.set("", ""));
+        Assert.assertEquals("Key should not be empty/blank", storage.set(" ", " "));
+
+        Assert.assertEquals("Key overwritten", storage.set(123, "one and two and three"));
+        Assert.assertTrue(storage.storage.containsKey(123));
+        Assert.assertEquals("OK", storage.set(345, "three-four-five"));
+        Assert.assertTrue(storage.storage.containsKey(345));
+
+        storage.deleteAll();
     }
 
     @Test
@@ -97,6 +119,16 @@ public class StorageTest {
         Assert.assertFalse(storage.storage.containsValue("one-two-three"));
         Assert.assertEquals(storage.get(listOfIntegers), storage.delete(listOfIntegers));
         Assert.assertFalse(storage.storage.containsValue(listOfStrings));
+        Assert.assertTrue(storage.storage.isEmpty());
+
+        // если ключ равен null, пустой или не существует
+
+        Assert.assertEquals("Key should not be null", storage.get(null));
+        Assert.assertEquals("Key should not be empty/blank", storage.get(" "));
+        Assert.assertEquals("Key should not be empty/blank", storage.get(""));
+        Assert.assertEquals("nil", storage.get("foo"));
+
+        storage.deleteAll();
     }
 
     @Test
@@ -118,5 +150,51 @@ public class StorageTest {
         storage.set(listOfIntegers, listOfStrings);
 
         storage.set(123, "one-two-three");
+
+        // создаем словарь с такими же данными
+
+        Map<Object, Object> expectedMap = new HashMap<>();
+        expectedMap.put("server:name", "fido");
+        expectedMap.put(listOfIntegers, listOfStrings);
+        expectedMap.put(123, "one-two-three");
+
+        // проверяем что метод keys не возвращает null в случае если storage заполнен данными
+
+        Assert.assertFalse(storage.keys().isEmpty());
+
+        // проверяем что метод keys возвращает искомый сет значений по ключу
+
+        Assert.assertEquals(expectedMap.keySet(), storage.keys());
+
+        storage.deleteAll();
+    }
+
+    @Test
+    public void deleteAll() {
+        Storage storage = new Storage();
+        storage.set("server:name", "fido");
+
+        List<Integer> listOfIntegers = new ArrayList<>();
+        listOfIntegers.add(1);
+        listOfIntegers.add(2);
+        listOfIntegers.add(3);
+
+        List<String> listOfStrings = new ArrayList<>();
+        listOfStrings.add("one");
+        listOfStrings.add("two");
+        listOfStrings.add("three");
+
+        storage.set(listOfIntegers, listOfStrings);
+
+        storage.set(123, "one-two-three");
+
+        // проверяем что storage не пустой
+
+        Assert.assertFalse(storage.storage.isEmpty());
+
+        // проверяем что после удаления storage пустой
+
+        storage.deleteAll();
+        Assert.assertTrue(storage.storage.isEmpty());
     }
 }
